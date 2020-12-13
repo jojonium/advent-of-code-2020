3457edc3101c0af271e233c9bafeb83079d1030b
Joseph Petitti <joseph@petitti.org>
2020-12-13 04:42:26 -0500

    finish day 13

--- day13.ts
+++ day13.ts
@@ -1,87 +1,95 @@
-import {linesAsStrings} from "./helpers";
+import { linesAsStrings } from "./helpers";
 import path from "path";
 
+/**
+ * Finds the amount of time you have to wait between your earliest departure and
+ * the next bus to arrive after.
+ */
 const timeToWait = (earliestDeparture: number, buses: number[]): number =>
-  buses.reduce((out, bus) => {
-    const arrival = earliestDeparture + bus - (earliestDeparture % bus)
-    if (arrival < out.wait) {
-      return {
-        id: bus,
-        wait: arrival,
-        answer: (arrival - earliestDeparture) * bus
-      };
-    }
-    return out;
-  }, {id: 0, wait: Infinity, answer: 0}).answer;
+  buses.reduce(
+    (out, bus) => {
+      const arrival = earliestDeparture + bus - (earliestDeparture % bus);
+      if (arrival < out.wait) {
+        return {
+          id: bus,
+          wait: arrival,
+          answer: (arrival - earliestDeparture) * bus,
+        };
+      }
+      return out;
+    },
+    { id: 0, wait: Infinity, answer: 0 }
+  ).answer;
 
 /**
+ * Returns the modulo inverse of a with repect to m.
+ */
+const multInverse = (a: number, m: number): number => {
+  const m0 = m;
+  let x0 = 0;
+  let x1 = 1;
+  if (m === 1) return 0;
+  // apply extended Euclid algorithm
+  while (a > 1) {
+    const q = Math.floor(a / m);
+    let t = m;
+    m = a % m;
+    a = t;
+    t = x0;
+    x0 = x1 - q * x0;
+    x1 = t;
+  }
+  if (x1 < 0) x1 += m0;
+  return x1;
+};
+
+/**
+ * Returns the smallest number x such that:
+ *   x % nums[i] === remainders[i]
+ * for all i in (0, nums.length).
+ */
+const chineseRemainderTheorem = (
+  nums: number[],
+  remainders: number[]
+): number => {
+  let sum = 0;
+  const prod = nums.reduce((a, b) => a * b);
+  for (let i = 0; i < nums.length; ++i) {
+    const p = Math.floor(prod / nums[i]);
+    sum += remainders[i] * multInverse(p, nums[i]) * p;
+  }
+  return sum % prod;
+};
+
+/**
+ * Finds the earliest timestamp such that all the listed bus IDs depart at
+ * offsets matching their positions in the list.
  * @param buses the bus ID's, with "x"s expressed as 0's
  */
-const subsequentDepartures = (buses: number[]) => {
-  const mods: {value: number, index: number}[] = [];
-  buses.forEach((b, i) => {
-    if (b !== 0) {
-      mods.push({value: b, index: i});
+const subsequentDepartures = (buses: number[]): number => {
+  const nums = new Array<number>();
+  const remainders = new Array<number>();
+  buses.forEach((bus, index) => {
+    if (bus !== 0) {
+      const rem = bus - index;
+      nums.push(bus);
+      remainders.push(rem);
     }
   });
-  mods.sort((a, b) => b.value - a.value);
-  let answer = mods[0].value + mods[0].index;
-  let increment = mods[0].value;
-  for (const mod of mods.slice(1)) {
-    answer = helper(mod.value, mod.index, answer, increment);
-    increment *= mod.value;
-  }
-  return increment - answer + buses[0];
-}
-
-const helper = (num: number, index: number, start: number, increment: number): number => {
-  console.log(`Helper: num=${num}, index=${index}, start=${start}, increment=${increment}`);
-  if (start % num === index) return start;
-  let acc = start;
-  while (acc % num !== index) acc += increment;
-  console.log(acc);
-  return acc;
-}
-
-/*
-const tester = (start: number) => {
-  let num = start;
-  while (true) {
-    num += 17;
-    if (num % 17 === 0 && num % 13 === 2 && num % 19 === 3) {
-      break;
-    }
-  }
-  return num;
-}
-*/
-
+  const out = chineseRemainderTheorem(nums, remainders);
+  return out;
+};
 
 (async () => {
   const input = await linesAsStrings(path.join(".", "inputs", "day13.txt"));
   const earliestDeparture = +input[0];
-  const buses = input[1].split(",").filter(s => s !== "x").map(s => parseInt(s))
+  const buses = input[1]
+    .split(",")
+    .filter((s) => s !== "x")
+    .map((s) => parseInt(s));
   console.log(`Part 1: ${timeToWait(earliestDeparture, buses)}`);
-  const busesPart2 = input[1].split(",").map(s => s === "x" ? 0 : parseInt(s));
-  if (busesPart2[0] === -1) { // TODO delete this
-    return;
-  }
-  console.log("Tests");
-  // console.log(subsequentDepartures([7, 13, 0, 0, 59, 0, 31, 19]) === 1068788);
-  console.log(subsequentDepartures([17, 0, 13, 19]) === 3417 + 17);
-  /*
-  console.log(subsequentDepartures([67, 7, 59, 61]) === 754018 + 67);
-  console.log(subsequentDepartures([67, 0, 7, 59, 61]) === 779210 + 67);
-  console.log(subsequentDepartures([67, 7, 0, 59, 61]) === 1261476 + 67);
-  console.log(subsequentDepartures([1789, 37, 47, 1889]) === 1202161486 + 1789);
-  */
-
-  // console.log(`Part 2: ${subsequentDepartures(busesPart2)}`);
-  /*
-  let k = 3417;
-  for (let i = 0; i < 5; ++i) {
-    k = tester(k);
-    console.log(k);
-  }
- */
+  const busesPart2 = input[1]
+    .split(",")
+    .map((s) => (s === "x" ? 0 : parseInt(s)));
+  console.log(`Part 2: ${subsequentDepartures(busesPart2)}`);
 })();
