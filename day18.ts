9d881a142ab8ffa7dbddec4ea43cb41996ea3ae5
Joseph Petitti <joseph@petitti.org>
2020-12-18 02:04:18 -0500

    clean up day 18

--- day18.ts
+++ day18.ts
@@ -37,59 +37,21 @@ const nextMultiplierIndex = (line: string, start: number): number => {
 };
 
 /**
- * Evaluates a string of +, *, and parenthesis with left-to-right operator
- * precedence.
- * Assumptions: number literals are all exactly one digit.
+ * Looks ahead to get the right side argument of a high-priority operator.
+ * @param index the index right after the operator
  */
-const evaluate = (line: string): number => {
-  let val;
-  let operator = "";
-  for (let i = 0; i < line.length; ++i) {
-    const char = line.charAt(i);
-    if (char === " " || char === ")") {
-      continue;
-    } else if (char === "+" || char === "*") {
-      operator = char;
-    } else if (/\d/.test(char)) {
-      // got a digit
-      const nextVal = parseInt(char);
-      if (val === undefined) val = nextVal;
-      else if (operator === "+") val += nextVal;
-      else if (operator === "*") val *= nextVal;
-      else throw new Error(`Invalid operator: '${operator}', i=${i}`);
-    } else if (char === "(") {
-      // got start of parenthetical
-      const closingIndex = closingParenIndex(line, i);
-      const nextVal = evaluate(line.substring(i + 1, closingIndex));
-      if (val === undefined) val = nextVal;
-      else if (operator === "+") val += nextVal;
-      else if (operator === "*") val *= nextVal;
-      else val = nextVal;
-      i = closingIndex + 1;
-    } else {
-      throw new Error(`Unexpected character: '${char}'`);
-    }
-  }
-  if (val === undefined) throw new Error("Something went really wrong!");
-  return val;
-};
-
-/**
- * @param index the index right after the +
- */
-const lookAheadPlus = (
+const lookAheadHigh = (
   line: string,
-  index: number
+  index: number,
+  p2Mode: boolean
 ): {nextVal: number, nextI: number} => {
   for (let i = index; i < line.length; ++i) {
     if (/\d/.test(line.charAt(i))) {
       const n = parseInt(line.charAt(i));
-      // console.log(`lookAheadPlus... got ${n}`);
       return {nextVal: n, nextI: i + 1};
     } else if (line.charAt(i) === "(") {
       const closingIndex = closingParenIndex(line, i);
-      const n = evaluateP2(line.substring(i + 1, closingIndex));
-      // console.log(`lookAheadPlus... got ${n}`);
+      const n = evaluate(line.substring(i + 1, closingIndex), p2Mode);
       return {
         nextVal: n,
         nextI: closingIndex + 1
@@ -100,25 +62,25 @@ const lookAheadPlus = (
 };
 
 /**
- * @param index the index right after the *
+ * Looks ahead to get the right side argument of a low-priority operator.
+ * @param index the index right after the operator
  */
-const lookAheadTimes = (
+const lookAheadLow = (
   line: string,
-  timesIndex: number
+  timesIndex: number,
+  p2Mode: boolean
 ): {nextVal: number, nextI: number} => {
   for (let i = timesIndex; i < line.length; ++i) {
     if (/\d/.test(line.charAt(i)) || line.charAt(i) === "(") {
       const closingIndex = nextMultiplierIndex(line, i - 1);
-      const n = evaluateP2(line.substring(i, closingIndex));
-      // console.log(`lookAheadTimes... got ${n}`);
+      const n = evaluate(line.substring(i, closingIndex), p2Mode);
       return {
         nextVal: n,
         nextI: closingIndex - 1
       };
     } else if (line.charAt(i) === "(") {
       const closingIndex = closingParenIndex(line, i);
-      const n = evaluateP2(line.substring(i + 1, closingIndex));
-      // console.log(`lookAheadTimes... got ${n}`);
+      const n = evaluate(line.substring(i + 1, closingIndex), p2Mode);
       return {
         nextVal: n,
         nextI: closingIndex + 1
@@ -128,40 +90,52 @@ const lookAheadTimes = (
   throw new Error("Shouldn't have gotten here (times)");
 };
 
-const evaluateP2 = (line: string): number => {
-  // console.log(`Evaluating '${line}'`);
-  let {nextVal: val, nextI: i} = lookAheadPlus(line, 0);
+/**
+ * Evaluates a line containing addition, multiplication, and parenthesis
+ * according to the precedence rules of the problem.
+ * @param p2Mode whether to consider + as higher operator priority than *
+ */
+const evaluate = (line: string, p2Mode = false): number => {
+  let {nextVal: val, nextI: i} = lookAheadHigh(line, 0, p2Mode);
   for (; i < line.length; ++i) {
     const char = line.charAt(i);
     if (char === " " || char === ")") {
       continue;
     } else if (char === "+") {
-      const ahead = lookAheadPlus(line, i + 1);
+      const ahead = lookAheadHigh(line, i + 1, p2Mode);
       val += ahead.nextVal;
       i = ahead.nextI;
     } else if (char === "*") {
-      const ahead = lookAheadTimes(line, i + 1);
+      let ahead;
+      if (p2Mode) {
+        ahead = lookAheadLow(line, i + 1, p2Mode);
+      } else {
+        ahead = lookAheadHigh(line, i + 1, p2Mode);
+      }
       val *= ahead.nextVal;
       i = ahead.nextI;
     }
   }
-  // console.log(`Returning ${val}`);
   return val;
 };
 
 (async () => {
   const input = await linesAsStrings(path.join(".", "inputs", "day18.txt"));
   const part1 = input.reduce((prev, cur) => prev + evaluate(cur), 0);
-  console.log(`Part 1: ${part1 === 5374004645253}`);
-  const part2 = input.reduce((prev, cur) => prev + evaluateP2(cur), 0);
+  console.log(`Part 1: ${part1}`);
+  const part2 = input.reduce((prev, cur) => prev + evaluate(cur, true), 0);
   console.log(`Part 2: ${part2}`);
-  // Tests:
   /*
-  console.log(evaluateP2("1 + (2 * 3) + (4 * (5 + 6))") === 51);
-  console.log(evaluateP2("2 * 3 + (4 * 5)") === 46);
-  console.log(evaluateP2("5 + (8 * 3 + 9 + 3 * 4 * 3)") === 1445);
-  console.log(evaluateP2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") === 669060);
-  console.log(evaluateP2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") === 23340);
-  console.log(evaluateP2("(5 * (4 * 7) + 6)") === 170);
-  */
+   * Tests
+  console.log(evaluate("2 * 3 + (4 * 5)") === 26);
+  console.log(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)") === 437);
+  console.log(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") === 12240);
+  console.log(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") === 13632);
+  console.log(evaluate("1 + (2 * 3) + (4 * (5 + 6))", true) === 51);
+  console.log(evaluate("2 * 3 + (4 * 5)", true) === 46);
+  console.log(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", true) === 1445);
+  console.log(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", true) === 669060);
+  console.log(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", true) === 23340);
+  console.log(evaluate("(5 * (4 * 7) + 6)", true) === 170);
+   */
 })();
